92df3cdeda292eeb1080908ef4a33dba4995ad7a
irinaoxymoron <irinaoxymoron@gmail.com>
2017-10-19 11:40:01 +0300

    Fix mmenu

--- dist/js/js-main.js
+++ dist/js/js-main.js
@@ -1,3 +1,22 @@
+$ (function () {
+    $('#my-menu').mmenu({
+        extensions: ['widescreen', 'theme-black', 'effect-menu-slide', 'pagedim-black'],
+        navbar: {
+            title: '<img src="img/logo-1.svg" alt="Салон красоты Смитлер">'
+        },
+        offCanvas: {
+            position: 'right'
+        }
+    });
+
+    var api = $('#my-menu').data('mmenu');
+    api.bind('opened', function () {
+        $('.hamburger').addClass('is-active');
+    }).bind('closed', function () {
+        $('.hamburger').removeClass('is-active');
+    });
+});
+
 $(window).on('load', function() {
     $('.preloader').delay(1000).fadeOut('slow');
 });
