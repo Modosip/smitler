fff09c285d77831a588023385fd4d5ada0b75b5f
irinaoxymoron <irinaoxymoron@gmail.com>
2017-10-19 18:06:26 +0300

    Section Services was added

--- dist/js/js-main.js
+++ dist/js/js-main.js
@@ -16,9 +16,51 @@ $ (function () {
         $('.hamburger').removeClass('is-active');
     });
 
-    $('.carousel-services').owlCarousel({
-
+    $('.carousel-services').on('initialized.owl.carousel', function () {
+        setTimeout(function () {
+            carouselService()
+        }, 100);
     });
+    $('.carousel-services').owlCarousel({
+        loop: true,
+        nav: true,
+        smartSpeed: 700,
+        navText: ['<i class="fa fa-angle-double-left"></i>', '<i class="fa fa-angle-double-right"></i>'],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            800: {
+                items: 2
+            },
+            1100: {
+                items: 3
+            }
+        }
+    });
+
+    function carouselService() {
+        $('.carousel-services__item').each(function () {
+            var ths = $(this);
+            var thsh = ths.find('.carousel-services__content').outerHeight();
+            ths.find('.carousel-services__image').css('min-height', thsh);
+        });
+    }carouselService();
+
+    $('.carousel-service__composition h3').each(function() {
+        var ths = $(this);
+        ths.html(ths.html().replace(/(\S+)\s*$/, '<span>$1</span>'));
+    });
+
+    //Resize Window
+    function onResize() {
+        $('.carousel-services__content').equalHeights();
+    }onResize();
+    window.onResize =function () {
+        onResize()
+    };
+
 });
 
 $(window).on('load', function() {
