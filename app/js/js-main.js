71e0d4c91a16bc0f1192b233b2bd0ee785896b17
irinaoxymoron <irinaoxymoron@gmail.com>
2017-10-23 17:01:38 +0300

    Fix footer

--- app/js/js-main.js
+++ app/js/js-main.js
@@ -85,6 +85,17 @@ $ (function () {
         sortField: 'text'
     });
 
+    $('.top').click(function() {
+        $('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+    });
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > $(window).height()) {
+            $('.top').addClass("active");
+        } else {
+            $('.top').removeClass("active");
+        };
+    });
+
     function carouselService() {
         $('.carousel-services__item').each(function () {
             var ths = $(this);
